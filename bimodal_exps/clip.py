eb71a7bd0534efbfa22341b6a0135541a6045a60
Khanh <legiakhanh27@gmail.com>
2025-11-27 20:27:35 -0600

    update

--- bimodal_exps/clip.py
+++ bimodal_exps/clip.py
@@ -536,7 +536,7 @@ def main(args):
                 
                 global average_score, current_best_checkpoint
                 if np.average(metrics) > average_score:
-                    if not current_best_checkpoint and os.path.exists(current_best_checkpoint):
+                    if current_best_checkpoint and os.path.exists(current_best_checkpoint):
                         # os.remove(args.checkpoint)
                         print(f"Delete checkpoint {current_best_checkpoint}")
 
