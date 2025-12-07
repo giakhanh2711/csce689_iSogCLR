da391b7e1ec2b50c102afbb14fb497443e506d4a
Gia Khanh Le <53686667+giakhanh2711@users.noreply.github.com>
2025-12-06 21:32:18 -0600

    Update losses.py

--- bimodal_exps/models/losses.py
+++ bimodal_exps/models/losses.py
@@ -28,6 +28,58 @@ class GatherLayer(torch.autograd.Function):
         grad_out[:] = grads[dist.get_rank()]
         return grad_out
 
+"""
+    VICReg
+    https://github.com/facebookresearch/vicreg/blob/main/main_vicreg.py
+"""
+def off_diagonal(x):
+    # return a flattened view of the off-diagonal elements of a square matrix
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+
+class VICReg_Loss(nn.Module):
+    def __init__(self, world_size, dim_size, sim_coeff=25.0, std_coeff=25.0, cov_coeff=1.0):
+        super(VICReg_Loss, self).__init__()
+
+        self.world_size = world_size
+        self.dim_size = dim_size
+        self.sim_coeff = sim_coeff
+        self.std_coeff = std_coeff
+        self.cov_coeff = cov_coeff
+
+
+    def forward(self, image_features, text_features):
+        if self.world_size > 1:
+            x = torch.cat(GatherLayer.apply(image_features), dim=0)
+            y = torch.cat(GatherLayer.apply(text_features), dim=0)
+
+        batch_size = len(x)
+
+        repr_loss = F.mse_loss(x, y) # invariance term
+
+        x = x - x.mean(dim=0)
+        y = y - y.mean(dim=0)
+
+        std_x = torch.sqrt(x.var(dim=0) + 0.0001)
+        std_y = torch.sqrt(y.var(dim=0) + 0.0001)
+        std_loss = torch.mean(F.relu(1 - std_x)) / 2 + torch.mean(F.relu(1 - std_y)) / 2  # variance term
+
+        cov_x = (x.T @ x) / (batch_size - 1)
+        cov_y = (y.T @ y) / (batch_size - 1)
+        cov_loss = off_diagonal(cov_x).pow_(2).sum().div(
+            self.dim_size
+        ) + off_diagonal(cov_y).pow_(2).sum().div(self.dim_size)  # covariance term
+
+        loss = (
+            self.sim_coeff * repr_loss
+            + self.std_coeff * std_loss
+            + self.cov_coeff * cov_loss
+        )
+
+        return loss
+
 
 class CLIP_Loss(nn.Module):
 
